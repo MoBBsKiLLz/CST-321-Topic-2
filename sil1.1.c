f8640b791ea0a1b9d31dd0ba94054ae0c4bf3544
Miguel Zepeda <migg_18@live.com>
2023-09-08 10:12:41 -0700

    Added sil1.1

---
+++ sil1.1.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void wakeup(int signum) {
+    for (int i = 1; i <= 20; i++) {
+        printf("Message %d from child.\n", i);
+        sleep(1);
+    }
+}
+
+void main() {
+    signal(SIGUSR1, wakeup);
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        while (1);
+        exit(EXIT_SUCCESS);
+    }
+    while (1) {
+        for (int i = 1; i <= 30; i++) {
+            printf("Message %d from parent.\n", i);
+            if (i == 5) {
+                kill((int)childpid, SIGUSR1);
+            }
+            sleep(1);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
