558e01a73a51b52724ad239ebb696d73a8b21dec
Miguel Zepeda <migg_18@live.com>
2023-09-08 15:09:19 -0700

    added til1.c

---
+++ til1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* show_message(void* arg) {
+    int *limit_ptr = (int*) arg;
+    int limit = *limit_ptr;
+
+    for (int i = 0; i <= 10; ++i) {
+        printf("This is message %d\n", i);
+        sleep(limit);
+    }
+}
+
+int main(int argc, char **argv) {
+    long long limit1 = atoll(argv[1]); 
+    
+    // Thread IDs
+    pthread_t tid1;
+    pthread_t tid2;
+
+    // Create attributes
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    // Create Threads
+    pthread_create(&tid1, &attr, show_message, 1);
+    pthread_create(&tid2, &attr, show_message, 2);
+
+    // Wait for threads to finish
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+}
