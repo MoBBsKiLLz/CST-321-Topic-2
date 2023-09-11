24324e838b7f390ca141437f5038999978846a3e
Miguel Zepeda <migg_18@live.com>
2023-09-10 17:33:36 -0700

    Updated semaphore

--- semaphore.c
+++ semaphore.c
@@ -1,53 +1,73 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 // Author: Miguel Zepeda
-#define THREAD_NUM 2
+#define THREAD_NUM 4
 
-int buffer[10];
+sem_t semEmpty;
+sem_t semFull;
+
+int restroom[2];
 int count = 0;
 
-pthread_mutex_t mutexBuffer;
+pthread_mutex_t mutexRestroom;
 
-void* producer(void* args) {
+void* business(void* args) {
     while(1) {
         // Produce random number
-        int x = rand() % 100;
+        int x = rand() % 2;
+        sleep(1);
 
-        pthread_mutex_lock(&mutexBuffer);
-        // Add to buffer
-        buffer[count] = x;
-        count = count + 1;
-        pthread_mutex_unlock(&mutexBuffer);
+        // Add to restroom
+        sem_wait(&semEmpty);
+        pthread_mutex_lock(&mutexRestroom);
+        if (count < 1) {
+            restroom[count] = x + 1;
+            count = count + 1;
+        } else {
+            printf("Skipped %d\n", x);
+        }
+        pthread_mutex_unlock(&mutexRestroom);
+        sem_post(&semFull);
     }
 }
 
-void* consumer(void* args) {
+void* customer(void* args) {
     while(1) {
-        // Remove from buffer
-        pthread_mutex_lock(&mutexBuffer);
-        int y = buffer[count - 1];
+        // Remove from restroom
+        sem_wait(&semFull);
+        pthread_mutex_lock(&mutexRestroom);
+        int y = restroom[count - 1];
         count = count - 1;
-        pthread_mutex_unlock(&mutexBuffer);
+        pthread_mutex_unlock(&mutexRestroom);
+        sem_post(&semEmpty);
 
         // Consume
-        printf("Got %d\n", y);
+        printf("Restroom %d is being used\n", y);
+        sleep(1);
     }
 }
 
 int main(int argc, char* argv[]) {
     srand(time(NULL));
-    // Create producer and consumer threads
+    // Create business and customer threads
     pthread_t th[THREAD_NUM];
-    pthread_mutex_init(&mutexBuffer, NULL);
+    pthread_mutex_init(&mutexRestroom, NULL);
+    sem_init(&semEmpty, 0, 1);
+    sem_init(&semFull, 0, 0);
     int i;
     for(i = 0; i < THREAD_NUM; i++) {
-        if(phtread_create(&th[i], NULL, &producer, NULL) != 0) {
-            perror("Failed to create thread");
+        if (i == 0) {
+            if(pthread_create(&th[i], NULL, &business, NULL) != 0) {
+                perror("Failed to create thread");
+            } 
         } else {
-            if(phtread_create(&th[i], NULL, &consumer, NULL) != 0) {
+            if(pthread_create(&th[i], NULL, &customer, NULL) != 0) {
             perror("Failed to create thread");
+            }
         }
     }
 
@@ -57,6 +77,8 @@ int main(int argc, char* argv[]) {
             perror("Failed to join thread");
         }
     }
-    pthread_mutex_destroy(&mutexBuffer);
+    sem_destroy(&semEmpty);
+    sem_destroy(&semFull);
+    pthread_mutex_destroy(&mutexRestroom);
     return 0;
 }
