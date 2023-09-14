3d27d497666645f7f03b8a388ef09a5ae8a59b29
Miguel Zepeda <migg_18@live.com>
2023-09-13 22:32:50 -0700

    still not running but here is the latest

--- msil.c
+++ msil.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 #define MAX_COUNT 200
 #define N 10
@@ -10,82 +13,70 @@ int writeIndx = 0;
 int readIndx = 0;
 int count = 0;
 
-int put(char item) {
-    if(count == N) {
+int ParentProcess(char letter) {
+
+    if(count == N - 1) {
         return 0; // Buffer is full
     }
-    buffer[writeIndx] = item;
+
+    buffer[writeIndx] = letter;
     writeIndx = (writeIndx + 1)  % N;
     count++;
+    
     return 1;
 }
-int get(int* item) {
+
+int ChildProcess(char* letter) {
     if (readIndx == writeIndx) {
-        return 0; // buffer is empty
+        return 0; // Buffer is empty
     }
 
-    *item = buffer[readIndx];
+    *letter = buffer[readIndx];
     readIndx = (readIndx + 1) % N;
     count--;
-    printf("Got the letter %s\n", &item);
+
+    printf("Got letter %c\n", *letter);
     return 1;
 }
 
-void ChildProcess(void) {
-    int i;
-
-    for (i = 1; i <= MAX_COUNT; i++) {
-        printf("  This line is from child, value = %d\n", i);
-    }
-    printf("  *** Child process is done ***\n");
-}
-
-void ParentProcess(void) {
-    int i;
-
-    for (i = 1; i <= MAX_COUNT; i++) {
-        printf("This line is from parent, value = %d\n", i);
-    }
-    printf("*** Parent is done ***\n");
-
-    char word[] = "Miguel";
-
-    int i = 0;
-    while(put(word[i])) {
-        i++;
+void charavail(int signum) {
+    for(int i = 0; i < count; i++) {
+        ChildProcess(&buffer[i]);
+        sleep(1);
     }
 }
 
-void main(void) {
-    pid_t pid;
+void complete(int signum) {
+    exit(1);
+}
 
-    pid = fork();
+void main() {
+    signal(SIGUSR1, charavail);
+    signal(SIGUSR2, complete);
+    pid_t pid = fork();
+
     if (pid == 0) {
-        ChildProcess();
-    } else {
-        ParentProcess();
-    }
-}void ChildProcess(void) {
-    int i;
+        for (;;);
+    } 
+    
+    while(1){
+        char word[] = "Miguel";
+        int length = sizeof(word) / sizeof(word[0]);
+        for (int i = 0; i < length; i++) {
+            ParentProcess(word[i]);
 
-    for (i = 1; i <= MAX_COUNT; i++) {
-        printf("  This line is from child, value = %d\n", i);
-    }
-    printf("  *** Child process is done ***\n");
-}
+            if (i == 1) {
+                kill(pid, SIGUSR1);
+                printf("Character available.\n");
+            } else if (i == length - 1) {
+                ParentProcess('\0');
+                kill(pid, SIGUSR2);
+                printf("End of string.\n");
+            }
 
-void ParentProcess(void) {
-    int i;
+            sleep(1);
+        }
 
-    for (i = 1; i <= MAX_COUNT; i++) {
-        printf("This line is from parent, value = %d\n", i);
-    }
-    printf("*** Parent is done ***\n");
-
-    char word[] = "Miguel";
-
-    int i = 0;
-    while(put(word[i])) {
-        i++;
-    }
+        exit(1);
+    }      
 }
